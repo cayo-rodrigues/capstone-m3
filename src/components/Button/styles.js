8eaf881627545e0268348d4ad74eb7bc4399df47
gustavo moura <g.gustavosouma@gmail.com>
2022-03-16 17:10:03 -0300

    feat: feature botao feita e funcional

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,4 +1,25 @@
 import styled from "styled-components";
 
-// lembre-se de usar html semântico
-export const Container = styled.div``;
+export const Container = styled.button`
+  background: ${(props) =>
+    props.isprimary === true
+      ? "var(--color-primary)"
+      : "var(--color-secondary)"};
+  color: ${(props) => props.color};
+  height: ${(props) => props.heigth};
+  border-radius: 26px;
+  font-size: 18px;
+  border: 2px solid
+    ${(props) =>
+      props.isprimary === true
+        ? "var(--color-primary)"
+        : "var(--color-secundary)"};
+  width: ${(props) => props.width};
+  opacity: ${(props) => props.active ? 1 : 0.5};
+  :hover {
+    border: 2px solid var(--color-primary-focus);
+  }
+  @media (max-width: 400px) {
+    width: ${(props) => props.mobile};
+  }
+`;
