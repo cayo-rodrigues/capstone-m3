5f3eca98ff11438744dce8f754a7cca86db051c4
christian-bm <christianbm1998@gmail.com>
2022-03-16 15:21:37 -0400

    feat: input criado

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,5 +1,38 @@
-const Input = () => {
-    return <div></div>;
+import { Container, Content } from "./styles";
+
+import { HiEye, HiEyeOff } from "react-icons/hi";
+import { useState } from "react";
+
+const Input = ({ label, type = "text", error, name, register, ...rest }) => {
+    const [typePassword, setTypePassword] = useState("password");
+
+    const handleTypePassword = () => {
+        if (typePassword === "text") {
+            setTypePassword("password");
+        } else {
+            setTypePassword("text");
+        }
+    };
+
+    return (
+        <Container>
+            <p>
+                {label} {!!error && <span> - {error}</span>}
+            </p>
+            <Content>
+                <input
+                    type={type === "password" ? typePassword : type}
+                    {...register(name)}
+                    {...rest}
+                />
+                {type === "password" && (
+                    <button onClick={() => handleTypePassword()}>
+                        {typePassword === "password" ? <HiEye /> : <HiEyeOff />}
+                    </button>
+                )}
+            </Content>
+        </Container>
+    );
 };
 
 export default Input;
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,4 +1,38 @@
 import styled from "styled-components";
 
 // lembre-se de usar html semântico
-export const Container = styled.div``;
+export const Container = styled.div`
+    p {
+        padding-left: 16px;
+        margin-bottom: 10px;
+    }
+`;
+
+export const Content = styled.div`
+    background: var(--color-secondary);
+    border: 2px solid var(--color-secondary);
+    padding: 8px 16px 12px;
+    border-radius: 35px;
+    box-shadow: 5px 4px 4px rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: space-between;
+    :focus-within {
+        border-color: var(--color-primary);
+    }
+    input {
+        background: var(--color-secondary);
+        border: none;
+        color: var(--white);
+        width: 100%;
+        font-size: 14px;
+    }
+    button {
+        margin-left: 10px;
+        background: transparent;
+        border: none;
+        color: var(--white);
+        svg {
+            transform: translateY(3px) scale(1.5);
+        }
+    }
+`;
