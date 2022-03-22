9decaf7c779cdb6d47809a4bed4ea05af5a713ae
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-03-22 13:11:29 -0300

    fix/refact: corrige bug no search e faz GET na api para pesquisar

--- src/components/CardBox/index.jsx
+++ src/components/CardBox/index.jsx
@@ -1,25 +1,20 @@
 import Card from "../Card";
 import { Container } from "./styles";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-
-
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 
 AOS.init({
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 1000, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
+  easing: "ease", // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 });
 
-
-const CardBox = ({workers}) => {
-
- 
+const CardBox = ({ workers }) => {
   const pessoas = [
     {
       nome: "Cirilão",
@@ -60,7 +55,7 @@ const CardBox = ({workers}) => {
         "Não aperte a mão dele",
         "Vou dar choque no seu sistema",
         "teste",
-        "teste2"
+        "teste2",
       ],
     },
     {
@@ -78,12 +73,12 @@ const CardBox = ({workers}) => {
   return (
     <Container>
       {workers.map((pessoa, index) => (
-        <Card 
+        <Card
           id={pessoa.id}
           key={pessoa.id}
-          img={''}
+          img={""}
           nome={pessoa.user.name}
-          especialidades={pessoa.occupation_area}
+          especialidades={pessoa.occupation_areas}
         />
       ))}
     </Container>
--- src/pages/SearchResult/index.jsx
+++ src/pages/SearchResult/index.jsx
@@ -1,31 +1,32 @@
 import { ServicePageContainer } from "./style";
-import home from "../../assets/svg/home.svg";
-import BottomNavigator from "../../components/BottomNavigator";
 
 import CardBox from "../../components/CardBox";
 import { useWorkers } from "../../providers/workers";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { proWorkingApi } from "../../services/api";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const SearchResults = () => {
-    const {workers} = useWorkers()
-    const {search} = useParams()
+  const { search } = useParams();
+  const [workersList, setWorkersList] = useState([]);
 
-    console.log()
-    const filteredWorkers = workers.filter(worker=> worker.occupation_area.includes(search) ) 
+  proWorkingApi
+    .get("/workers", {
+      params: {
+        _expand: "user",
+        _embed: "ratings",
+        q: search.toLowerCase().trim(),
+      },
+    })
+    .then((res) => {
+      setWorkersList(res.data);
+    });
+
   return (
     <ServicePageContainer>
-      {/* <Container>
-        <h1>
-          Bem vindo ao <span>PROWORKING</span>
-        </h1>
-        <p>Encontre o serviço que você precisa aqui!</p>
-        <Input isSearch />
-        <Button />
-        <img src={home} alt="homeimg" />
-      </Container> */}
-      <h1>Resultados para a pesquisa {search} </h1>
-      <CardBox workers={filteredWorkers}/>
-
+      <h1>Resultados para a pesquisa "{search}" </h1>
+      <CardBox workers={workersList} />
     </ServicePageContainer>
   );
 };
