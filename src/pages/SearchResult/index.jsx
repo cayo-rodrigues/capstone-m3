8b0ad77e058f0bf4d74be7a9aeb3544b6bcbc0d6
Gabriel Rocha <gabrielrs7@outlook.com>
2022-03-22 19:06:11 -0300

    fix: corrigindo bug no search result

--- src/pages/SearchResult/index.jsx
+++ src/pages/SearchResult/index.jsx
@@ -1,31 +1,23 @@
 import { ServicePageContainer } from "./style";
-import home from "../../assets/svg/home.svg";
-import BottomNavigator from "../../components/BottomNavigator";
 
 import CardBox from "../../components/CardBox";
 import { useWorkers } from "../../providers/workers";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 const SearchResults = () => {
-    const {workers} = useWorkers()
-    const {search} = useParams()
-
-    console.log()
-    const filteredWorkers = workers.filter(worker=> worker.occupation_area.includes(search) ) 
+  const { workers } = useWorkers();
+  const { search } = useParams();
+  console.log(workers);
+  const filteredWorkers = workers.filter(
+    (worker) =>
+      worker.is_worker &&
+      worker.cities &&
+      worker.occupation_areas.some((area) => area.includes(search))
+  );
   return (
     <ServicePageContainer>
-      {/* <Container>
-        <h1>
-          Bem vindo ao <span>PROWORKING</span>
-        </h1>
-        <p>Encontre o serviço que você precisa aqui!</p>
-        <Input isSearch />
-        <Button />
-        <img src={home} alt="homeimg" />
-      </Container> */}
       <h1>Resultados para a pesquisa {search} </h1>
-      <CardBox workers={filteredWorkers}/>
-
+      <CardBox workers={filteredWorkers} />
     </ServicePageContainer>
   );
 };
