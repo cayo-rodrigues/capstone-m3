b743661848c0bfa22cf12df8d7529a007f7eda33
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-03-23 08:43:58 -0300

    refact: remove "\" na linha 153 do dashboard, logo antes do "-"

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -150,7 +150,7 @@ const Dashboard = () => {
                       setWhatsApp(e.target.value);
                       if (
                         !!e.target.value.match(
-                          /^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/
+                          /^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}-?[0-9]{4}$/
                         )
                       ) {
                         setWrongNumber(false);
