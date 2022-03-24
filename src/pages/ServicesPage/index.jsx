5b4456070d182dfae0b96a98b238f939b58263ad
Gabriel Rocha <gabrielrs7@outlook.com>
2022-03-23 22:06:19 -0300

    feature/search-Component-Created-In-Page-Service

--- src/pages/ServicesPage/index.jsx
+++ src/pages/ServicesPage/index.jsx
@@ -1,22 +1,89 @@
-import { ServicePageContainer } from "./styles";
-
+import { ServicePageContainer, SearchCity } from "./styles";
+import { AiOutlineSearch } from "react-icons/ai";
 import CardBox from "../../components/CardBox";
 import { useWorkers } from "../../providers/workers";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+
+AOS.init({
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
 
 const ServicesPage = () => {
   const { workers, refreshWorkers } = useWorkers();
+
+  const [filterWorkers, setFilterWorkers] = useState(workers);
+  const [search, setSearch] = useState("");
+
+  const [searchTitle, setSearchTitle] = useState("");
+
+  const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
     refreshWorkers();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleSearch = (text) => {
+    const userInput = text
+      .toLowerCase()
+      .split(" ")
+      .filter((elem) => elem !== " " || elem !== "")
+      .join(" ");
+
+    setClicked(true);
+    setSearchTitle(userInput)
+    setFilterWorkers(
+      workers.filter(worker=>worker.is_active).filter(
+        (worker)=>worker.user.name.toLowerCase().includes(userInput) ||
+          worker.summary.toLowerCase().includes(userInput) ||
+          worker.cities.filter(
+            ({ state, city }) =>
+              city.toLowerCase().includes(userInput) ||
+              state.toLowerCase().includes(userInput)
+          ).length ||
+          worker.occupation_areas.filter((occupation) =>
+            occupation.toLowerCase().includes(userInput)
+          ).length
+     )
+    );
+  };
+
   return (
-    <ServicePageContainer>
-      <h1>Todos os serviços</h1>
-      <CardBox workers={workers.filter(({ is_active }) => is_active)} />
-    </ServicePageContainer>
+    <>
+      <SearchCity>
+        <div data-aos="zoom-in" className=" container">
+          <h1>Pesquisar</h1>
+          <div className="linha"></div>
+          <div className="inputDiv">
+            <input
+              onChange={(e) => {
+                setSearch(e.target.value);
+                if(e.target.value.length===0) handleSearch('')
+              }}
+              placeholder="Pesquisar cidade ou serviço"
+            />
+            <button onClick={() => handleSearch(search)}>
+              <AiOutlineSearch />
+            </button>
+          </div>
+          {searchTitle && clicked && <span>Pesquisa para "{search}"</span>}
+        </div>
+      </SearchCity>
+
+      <ServicePageContainer>
+        <h2>Todos os serviços</h2>
+        <CardBox workers={filterWorkers.filter(({ is_active }) => is_active)} />
+      </ServicePageContainer>
+    </>
   );
 };
 
--- src/pages/ServicesPage/styles.js
+++ src/pages/ServicesPage/styles.js
@@ -10,7 +10,7 @@ export const Container = styled.div`
 
 export const ServicePageContainer = styled.main`
   margin: 60px auto;
-  h1 {
+  h2 {
     font-size: 40px;
     margin: 30px 0;
     text-align: center;
@@ -19,3 +19,73 @@ export const ServicePageContainer = styled.main`
     }
   }
 `;
+export const SearchCity = styled.div`
+  width: 100%;
+  background-color: #eceff1;
+  text-align: center;
+  .linha{
+    width: 100px;
+    margin: 10px auto 30px;
+    height: 3px;
+    background-color: var(--color-primary);
+  }
+  h1{
+    font-size: 40px;
+    @media (max-width: 768px) {
+      font-size: 23px;
+    }
+  }
+  .container{
+    width: 100%;
+    max-width: 900px;
+    padding: 90px 0;
+    margin: 0px auto;
+    min-height: 380px;
+    @media (max-width: 768px) {
+      padding: 40px 0;
+    }
+  }
+  .inputDiv{
+    max-width: 90%;
+    margin:20px auto ;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    button,input{
+      border: 0;
+      padding: 15px 20px;
+    }
+    input{
+      width: 90%;
+      height: 100%;
+      font-size: 20px;
+      border: 2px solid transparent;
+      border-right: 0;
+      :hover,:focus{
+        border: 2px solid #ddd;
+        border-right: 0;
+
+      }
+
+    }
+    button{
+      background-color: var(--color-primary);
+      color: white;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      transition: 1s all;
+      justify-content: center;
+      :hover{
+        transition: 1s all;
+        background-color: var(--color-aux);
+      }
+      svg{
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+  }
+`
