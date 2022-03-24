63308fa0ddfb5adbcab302372a433d96bc51271d
Gabriel Rocha <gabrielrs7@outlook.com>
2022-03-23 22:24:54 -0300

    feat: adding new pages to dashboard

--- src/pages/Dashboard/InitialPage/index.jsx
+++ src/pages/Dashboard/InitialPage/index.jsx
@@ -1,17 +1,46 @@
-import { Link } from 'react-router-dom'
-import { DashboardContainer } from './style';
+import { Link } from "react-router-dom";
+import { DashboardContainer } from "./style";
 
-const DashboardHomePage = ()=>{
+import Cadastro from "../../../assets/svg/cadastrar-ou-atualizar.svg";
+import VerAnúncios from "../../../assets/svg/see-all-jobs.svg";
+import CriarAnuncios from "../../../assets/svg/create.svg";
+import { Redirect } from "react-router-dom";
+import { useAuthenticated } from "../../../providers/authenticated";
+const DashboardHomePage = () => {
+  const profile = JSON.parse(localStorage.getItem("@ProWorking:user"));
 
-    const profile = JSON.parse(localStorage.getItem("@ProWorking:user"));
-    console.log(profile)
+  const {authenticated} = useAuthenticated()
 
-    return (
-        <DashboardContainer>
-            <h1>{profile.name} </h1>
+  if (!authenticated) {
+    return <Redirect to={"/"} />;
+  }
+  return (
+    <DashboardContainer>
+      <h1>Seja bem vindo {profile.name}!! 😊 </h1>
+      <h2>O que você deseja?</h2>
+      <ul>
+        <Link to={"/dashboard/service-register"}>
+          <li>
+            <img src={Cadastro} alt="Cadastrar" />
+            <h3>Cadastrar serviço ou Atualizar perfil</h3>
+          </li>
+        </Link>
 
-            <Link to={'/dashboard/service-register'}>Cadastrar serviço</Link>
-        </DashboardContainer>
-    )
-}
-export default DashboardHomePage
+        <Link to={"/dashboard/jobs-register"}>
+          <li>
+            <img src={CriarAnuncios} alt="Criar serviço" />
+            <h3>Criar Anúncio para Contratar Alguém</h3>
+          </li>
+        </Link>
+
+        <Link to={"/dashboard/all-jobs"}>
+          <li>
+            <img src={VerAnúncios} alt="Ver anúncios" />
+            <h3>Ver anúncios de Serviço</h3>
+          </li>
+        </Link>
+      </ul>
+    </DashboardContainer>
+  );
+};
+export default DashboardHomePage;
--- src/pages/Dashboard/InitialPage/style.js
+++ src/pages/Dashboard/InitialPage/style.js
@@ -1,7 +1,55 @@
 import styled from "styled-components";
 
 export const DashboardContainer = styled.main`
-    margin: 40px auto;
-    max-width: 1400px;
-    padding: 20px;
-`
+  margin: 40px auto;
+  max-width: 1400px;
+  padding: 30px;
+  text-align: center;
+  transition: 1s all;
+  animation: fadeIn 1s;
+  ul {
+    margin: 80px 0;
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    li {
+        max-height: 320px;
+      max-width: 250px;
+      padding: 0 0 40px;
+      margin: 10px 30px;
+      border: 2px solid #dadada;
+      transition: 1s all;
+      border-radius: 10% 10% 20px 20px ;
+      color:#000;
+      background-color: #f3f3f3;
+      font-weight: bold;
+      font-family: var(--font-text);
+      a{
+        color:#000;
+      }
+      :hover{
+          transform: scale(1.1);
+          transition: 1s all;
+
+      }
+      img {
+        width: 250px;
+        height: 250px;
+        object-position: left;
+        object-fit: cover;
+        border: 1px solid #eee;
+        background-color: #dadada;
+        transition: 1s all;
+        border-radius:10% 10% 0 0 ;
+        :hover{
+        }
+      }
+      h3 {
+        margin: 5px 0 0;
+        padding: 0 15px;
+        font-size: 15px;
+      }
+    }
+  }
+`;
