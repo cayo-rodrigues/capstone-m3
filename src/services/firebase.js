28362071d980c019ccc837660cbce95d0a7fe235
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-03-25 07:58:44 -0300

    fix: coloca autenticação do igor no firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzKlYp1Q4-mJEepOqMf6PidPiz6y718zQ",
-  authDomain: "auth-proworking.firebaseapp.com",
-  projectId: "auth-proworking",
-  storageBucket: "auth-proworking.appspot.com",
-  messagingSenderId: "80830929381",
-  appId: "1:80830929381:web:2d4d637e01b2f82f98770c",
+  apiKey: "AIzaSyBQEmb4VsWvulLrMYqGUCd2cxs2DouKFtM",
+  authDomain: "capstone-m3.firebaseapp.com",
+  projectId: "capstone-m3",
+  storageBucket: "capstone-m3.appspot.com",
+  messagingSenderId: "308594535655",
+  appId: "1:308594535655:web:5b3ba823faaf5de2b90642",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
